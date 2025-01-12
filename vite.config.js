43b0923c3fad7ef93ffd57d89bf6572c1352f17b
Xavier Cho <mysticfallband@gmail.com>
2025-01-12 22:12:00 +0900

    Fix build output and configuration problems
    Updated build process to generate both ESM and CJS outputs, replacing the single UMD target. Integrated glob module for dynamic entry mapping and adjusted package exports accordingly. Added `declarationMap` support for TypeScript and updated dependencies in the lockfile.

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,38 @@
-// noinspection JSUnusedGlobalSymbols
-
 import {defineConfig} from "vite"
 import path from "path"
 import tsconfigPaths from "vite-tsconfig-paths"
+import {globSync} from "glob"
+import {fileURLToPath} from "node:url"
 
 export default defineConfig({
     plugins: [tsconfigPaths()],
     build: {
-        lib: {
-            entry: path.resolve(__dirname, "src/index.ts"),
-            name: "skyrim-effect",
-            fileName: format => `skyrim-effect.${format}.js`
-        }, rollupOptions: {
+        rollupOptions: {
+            input: Object.fromEntries(
+                globSync("src/**/*.ts").map(file => [
+                    path.relative(
+                        "src",
+                        file.slice(0, file.length - path.extname(file).length)
+                    ),
+                    fileURLToPath(new URL(file, import.meta.url))
+                ])
+            ),
+            output: [
+                {
+                    entryFileNames: "[name].js",
+                    chunkFileNames: "[name]-[hash].js",
+                    format: "esm",
+                    dir: "dist/esm"
+                },
+                {
+                    entryFileNames: "[name].js",
+                    chunkFileNames: "[name]-[hash].js",
+                    format: "cjs",
+                    dir: "dist/cjs"
+                }
+            ],
             external: ["@skyrim-platform/skyrim-platform"],
-            output: {
-                globals: {}
-            }
-        }
-    },
-    test: {
-        coverage: {
-            provider: "v8",
-            reporter: ["json-summary", "html"],
-            exclude: ["docs"]
+            preserveEntrySignatures: "allow-extension"
         }
     }
 })

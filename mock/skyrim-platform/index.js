5b59e84050494445a56e3bfcceaeb1f4d826487d
Xavier Cho <mysticfallband@gmail.com>
2025-02-15 20:35:58 +0900

    Mock Skyrim Platform dependency in pnpm overrides.
    Replaces the `@skyrim-platform/skyrim-platform` package with a local mocked version using pnpm overrides. This allows testing or development without relying on the external dependency. Added mock implementation, type definitions, and updated package configurations.

---
+++ mock/skyrim-platform/index.js
@@ -0,0 +1,175 @@
+export class Action {}
+
+export class Activator {}
+
+export class ActiveMagicEffect {}
+
+export class Actor {}
+
+export class ActorBase {}
+
+export class ActorValueInfo {}
+
+export class Alias {}
+
+export class Ammo {}
+
+export class Apparatus {}
+
+export class Armor {}
+
+export class ArmorAddon {}
+
+export class Art {}
+
+export class AssociationType {}
+
+export class Book {}
+
+export class Cell {}
+
+export class Class {}
+
+export class ColorForm {}
+
+export class CombatStyle {}
+
+export class ConstructibleObject {}
+
+export class Container {}
+
+export class DefaultObjectManager {}
+
+export class Door {}
+
+export class EffectShader {}
+
+export class Enchantment {}
+
+export class EncounterZone {}
+
+export class EquipSlot {}
+
+export class Explosion {}
+
+export class Faction {}
+
+export class Flora {}
+
+export class Form {}
+
+export class FormList {}
+
+export class Furniture {}
+
+export class Game {}
+
+export class GlobalVariable {}
+
+export class Hazard {}
+
+export class HeadPart {}
+
+export class Idle {}
+
+export class ImageSpaceModifier {}
+
+export class ImpactDataSet {}
+
+export class Ingredient {}
+
+export class Key {}
+
+export class Keyword {}
+
+export class LeveledActor {}
+
+export class LeveledItem {}
+
+export class LeveledSpell {}
+
+export class Light {}
+
+export class Location {}
+
+export class LocationAlias {}
+
+export class LocationRefType {}
+
+export class MagicEffect {}
+
+export class Message {}
+
+export class MiscObject {}
+
+export class MusicType {}
+
+export class NetImmerse {}
+
+export class ObjectReference {}
+
+export class Outfit {}
+
+export class Package {}
+
+export class PapyrusObject {}
+
+export class Perk {}
+
+export class Potion {}
+
+export class Projectile {}
+
+export class Quest {}
+
+export class Race {}
+
+export class ReferenceAlias {}
+
+export class Scene {}
+
+export class Scroll {}
+
+export class ShaderParticleGeometry {}
+
+export class Shout {}
+
+export class SoulGem {}
+
+export class Sound {}
+
+export class SoundCategory {}
+
+export class SoundDescriptor {}
+
+export class Spell {}
+
+export class Static {}
+
+export class TextureSet {}
+
+export class Topic {}
+
+export class TopicInfo {}
+
+export class TreeObject {}
+
+export class Ui {}
+
+export class Utility {
+    static getCurrentGameTime() {
+        return 0
+    }
+}
+
+export class VisualEffect {}
+
+export class VoiceType {}
+
+export class Weapon {}
+
+export class Weather {}
+
+export class WordOfPower {}
+
+export class WorldSpace {}

b6c6dd00ed6c0eed00aa7f9f339fd5c8649b0c96
Xavier Cho <mysticfallband@gmail.com>
2025-03-31 23:55:26 +0900

    Add Rollup plugins for external dependency bundling
    Integrated `@rollup/plugin-node-resolve`, `rollup-plugin-external-globals`, and `rollup-plugin-node-externals` for better bundling of external dependencies in Rollup. Updated Vite configuration to include these plugins, enable conditional builds based on mode, and improve support for external modules like `@skyrim-platform/skyrim-platform`. Adjusted scripts in `package.json` to include a debug build mode.

--- vite.config.js
+++ vite.config.js
@@ -3,44 +3,67 @@ import path from "path"
 import tsconfigPaths from "vite-tsconfig-paths"
 import {globSync} from "glob"
 import {fileURLToPath} from "node:url"
+import externalGlobals from "rollup-plugin-external-globals"
+import nodeExternals from "rollup-plugin-node-externals"
+import nodeResolve from "@rollup/plugin-node-resolve"
 
-export default defineConfig({
-    plugins: [tsconfigPaths()],
-    build: {
-        rollupOptions: {
-            input: Object.fromEntries(
-                globSync("src/**/*.ts").map(file => [
-                    path.relative(
-                        "src",
-                        file.slice(0, file.length - path.extname(file).length)
-                    ),
-                    fileURLToPath(new URL(file, import.meta.url))
-                ])
-            ),
-            output: [
-                {
-                    entryFileNames: "[name].js",
-                    chunkFileNames: "[name]-[hash].js",
-                    format: "esm",
-                    dir: "dist/esm"
-                },
-                {
-                    entryFileNames: "[name].js",
-                    chunkFileNames: "[name]-[hash].js",
-                    format: "cjs",
-                    dir: "dist/cjs"
-                }
-            ],
-            external: ["@skyrim-platform/skyrim-platform"],
-            preserveEntrySignatures: "allow-extension"
-        }
-    },
-    test: {
-        include: ["./test/**/*.test.ts"],
-        coverage: {
-            provider: "v8",
-            reporter: ["json-summary", "html"],
-            exclude: ["docs"]
+export default defineConfig(({mode}) => {
+    const isDebug = mode === "development"
+
+    return {
+        plugins: [
+            tsconfigPaths(),
+            nodeExternals({
+                deps: false,
+                peerDeps: true
+            }),
+            externalGlobals({
+                "@skyrim-platform/skyrim-platform": "skyrimPlatform"
+            })
+        ],
+        build: {
+            rollupOptions: {
+                input: Object.fromEntries(
+                    globSync("src/**/*.ts").map(file => [
+                        path.relative(
+                            "src",
+                            file.slice(
+                                0,
+                                file.length - path.extname(file).length
+                            )
+                        ),
+                        fileURLToPath(new URL(file, import.meta.url))
+                    ])
+                ),
+                output: [
+                    {
+                        entryFileNames: "[name].js",
+                        chunkFileNames: "[name]-[hash].js",
+                        format: "esm",
+                        dir: "dist/esm"
+                    },
+                    {
+                        entryFileNames: "[name].js",
+                        chunkFileNames: "[name]-[hash].js",
+                        format: "cjs",
+                        dir: "dist/cjs"
+                    }
+                ],
+                external: ["skyrimPlatform", "effect"],
+                preserveEntrySignatures: "allow-extension",
+                plugins: [nodeResolve()]
+            },
+            minify: !isDebug,
+            sourcemap: isDebug,
+            target: "esnext"
+        },
+        test: {
+            include: ["./test/**/*.test.ts"],
+            coverage: {
+                provider: "v8",
+                reporter: ["json-summary", "html"],
+                exclude: ["docs"]
+            }
         }
     }
 })
